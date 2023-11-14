82dce6cc2097abb8ae0e34802f235f59cf4029db
Dušan <duci.sudjic@gmail.com>
2023-11-14 20:52:41 +0100

    fix: Controllers adjusted to front-end application

--- backend/src/main/java/ftn/isa/controller/UserController.java
+++ backend/src/main/java/ftn/isa/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "api/users")
 public class UserController {
     @Autowired
