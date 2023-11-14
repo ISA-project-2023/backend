0c8e578a15ef4226065c3146ad1a5af60e43a977
Dušan <duci.sudjic@gmail.com>
2023-11-14 20:53:19 +0100

    fix: CompanyController adjusted to front-end application

--- backend/src/main/java/ftn/isa/controller/CompanyController.java
+++ backend/src/main/java/ftn/isa/controller/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "api/companies")
 public class CompanyController {
     @Autowired
