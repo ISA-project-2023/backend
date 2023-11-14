75ccb24207b7e1f2d97f3563915716f7a06bb4ea
Dušan <duci.sudjic@gmail.com>
2023-11-14 22:52:39 +0100

    fix: configuration for frontend added.

---
+++ backend/src/main/java/ftn/isa/config/WebConfig.java
@@ -0,0 +1,21 @@
+package ftn.isa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow CORS for all endpoints
+                .allowedOrigins("http://localhost:4200")  // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("Content-Type", "Authorization")  // Allowed headers
+                .allowCredentials(true);  // Allow credentials (cookies)
+    }
+}
+
