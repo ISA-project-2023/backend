ac43db67b9fea8fd2097101791e47b1d42798ef5
Lara <larapetkovic021@gmail.com>
2023-12-15 21:50:36 +0100

    feat: QR code implemented on a registration confirmation.

--- backend/src/main/java/ftn/isa/controller/ReservationController.java
+++ backend/src/main/java/ftn/isa/controller/ReservationController.java
@@ -1,13 +1,11 @@
 package ftn.isa.controller;
 
-import ftn.isa.domain.CompanyAdmin;
-import ftn.isa.domain.PickUpAppointment;
-import ftn.isa.domain.Reservation;
+import ftn.isa.domain.*;
 import ftn.isa.dto.CompanyAdminDTO;
 import ftn.isa.dto.PickUpAppointmentDTO;
 import ftn.isa.dto.ReservationDTO;
 import ftn.isa.service.CompanyAdminService;
-import ftn.isa.domain.ReservationStatus;
+import ftn.isa.service.EmailService;
 import ftn.isa.service.PickUpAppointmentService;
 import ftn.isa.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -29,6 +28,9 @@ public class ReservationController {
 
     @Autowired
     private PickUpAppointmentService pickupService;
+
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping(value = "/all")
     public ResponseEntity<List<ReservationDTO>> getAllReservations() {
@@ -71,9 +73,83 @@ public class ReservationController {
         pua = pickupService.save(pua);
         reservation.setPickUpAppointment(pua);
 
-        reservation = service.save(reservation);
+        String to = reservationDto.getCustomer().getEmail();
+        String subject = "Reservation Confirmation";
+        String body = generateEmailBody(reservationDto);
+
+        // Include reservation details in the QR code
+        String qrCodeText = generateQRCodeText(reservationDto);
+
+        try {
+            emailService.sendWithQRCode(to, body, subject, qrCodeText);
+            System.out.println("Reservation information email sent successfully!");
+            reservation = service.save(reservation);
+        } catch (MessagingException e) {
+            System.out.println("Exception sending email: " + e.toString());
+        }
+
         return new ResponseEntity<>(new ReservationDTO(reservation), HttpStatus.CREATED);
     }
+
+    private String generateEmailBody(ReservationDTO reservationDto) {
+        String customerName = getCustomerName(reservationDto.getCustomer());
+
+        StringBuilder mail = new StringBuilder("<html>"
+                + "<body>"
+                + "<p>Dear " + customerName + ",</p>"
+                + "<p>Thank you for making a reservation in our service. Here are the details:</p>"
+                + "<ul>"
+                + "<li><strong>Company:</strong> " + reservationDto.getCompany().getName() + "</li>"
+                + "<li><strong>Equipment:</strong> <br/>");
+
+        for (Equipment e : reservationDto.getEquipment()) {
+            mail.append(e.getName()).append(" (").append(e.getDescription()).append(")").append("<br/>");
+        }
+
+        mail.append("</li>")
+                .append("<li><strong>Pick-up Date:</strong> ").append(reservationDto.getPickUpAppointment().getDate().toString().replace("T", " ")).append("</li>")
+                .append("<li><strong>Pick-up duration:</strong> ").append(reservationDto.getPickUpAppointment().getDuration()).append("</li>")
+                .append("</ul>")
+                .append("<p>Please find attached QR code for more details.</p>")
+                .append("<p>Best regards,<br/>The Isa Project Team</p>")
+                .append("</body>")
+                .append("</html>");
+
+        return mail.toString();
+    }
+
+
+    private String getCustomerName(User customer) {
+        if (customer instanceof Customer) {
+            Customer customerObj = (Customer) customer;
+            return customerObj.getFirstName() + " " + customerObj.getLastName();
+        } else {
+            // Handle the case where the provided user is not a Customer
+            return "Valued Customer";
+        }
+    }
+
+    private String generateQRCodeText(ReservationDTO reservationDto) {
+        String customerName = getCustomerName(reservationDto.getCustomer());
+        return "Reservation Details:\n"
+                + "Company: " + reservationDto.getCompany().getName() + "\n"
+                + "Customer: " + customerName + "\n"
+                + "Equipment: " + getEquipmentDetails(reservationDto.getEquipment()) + "\n"
+                + "Pick-up Date: " + reservationDto.getPickUpAppointment().getDate() + "\n";
+    }
+
+    private String getEquipmentDetails(List<Equipment> equipmentList) {
+        StringBuilder equipmentDetails = new StringBuilder();
+        for (Equipment e : equipmentList) {
+            equipmentDetails.append(e.getName()).append(" (").append(e.getDescription()).append("), ");
+        }
+        // Remove the trailing comma and space
+        if (equipmentDetails.length() > 0) {
+            equipmentDetails.setLength(equipmentDetails.length() - 2);
+        }
+        return equipmentDetails.toString();
+    }
+
 
     @GetMapping(value = "/findByCompanyAdmin/{id}")
     public ResponseEntity<List<ReservationDTO>> getAppointmentsByCompanyAdmins(@PathVariable Integer id) {
