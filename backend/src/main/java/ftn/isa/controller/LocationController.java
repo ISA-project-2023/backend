b299eeca3519ac74557059a4f549bcd314bd2251
Lara <larapetkovic021@gmail.com>
2024-01-28 19:01:23 +0100

    feat: WebSockets implemented

--- backend/src/main/java/ftn/isa/controller/LocationController.java
+++ backend/src/main/java/ftn/isa/controller/LocationController.java
@@ -1,29 +1,31 @@
 package ftn.isa.controller;
 
-import ftn.isa.domain.LocationConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.ErrorManager;
-
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "api/location")
 public class LocationController {
 
-    private static final Logger log = LoggerFactory.getLogger(LocationConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(LocationController.class);
     private static RestTemplate restTemplate = new RestTemplate();
+    private final SimpMessagingTemplate messagingTemplate;
 
     @Autowired
-    public LocationController(RestTemplate rt) {
-        restTemplate = rt;
+    public LocationController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
     }
 
     @PostMapping(value="/send-message", consumes="text/plain")
     public void sendRequestToOtherApp(@RequestBody String text) {
+        // Send the message via WebSocket I WANT THIS MESSAGE TO BE SENT
+        messagingTemplate.convertAndSend("/topic/location", text);
+
         String url = "http://localhost:8080/api/myexchange/spring-boot2";
         restTemplate.postForObject(url, text, String.class);
     }
@@ -36,5 +38,4 @@ public class LocationController {
             log.error("Error sending message to external app: " + e.getMessage());
         }
     }
-
 }
