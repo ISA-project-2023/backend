79d1a86fd209b4508fc2b5f95ca59890ab49c641
Dušan <duci.sudjic@gmail.com>
2024-01-31 19:11:40 +0100

    feat: raft example project

---
+++ RaftLib/RaftClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaftLib;
+
+namespace RaftClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Testing 
+            Console.Write("Enter id number to shutdown the leader server or enter to close this window:");
+
+            int number = int.Parse(Console.ReadLine());
+            string serviceAddress;
+            string serviceName;
+            switch (number)
+            {
+                case 1:
+                    serviceAddress = "127.0.0.1:4001";
+                    serviceName = "RaftService1";
+                    break;
+                case 2:
+                    serviceAddress = "127.0.0.1:4002";
+                    serviceName = "RaftService2";
+                    break;
+                default:
+                    serviceAddress = "127.0.0.1:4003";
+                    serviceName = "RaftService3";
+                    break;
+            }
+             
+
+            Uri tcpUri = new Uri($"net.tcp://{serviceAddress}/{serviceName}");
+
+            EndpointAddress adress = new EndpointAddress(tcpUri);
+
+            NetTcpBinding clientBinding = new NetTcpBinding();
+
+            ChannelFactory<IService1> factory = new ChannelFactory<IService1>(clientBinding, adress);
+
+            IService1 service = factory.CreateChannel();
+            service.ShutDownLeader();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RaftLib/RaftLib/IService1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Timers;
+
+namespace RaftLib
+{
+    [ServiceContract]
+    
+    public interface IService1
+    {
+        [OperationContract]
+        void SendVoteRequest();
+        [OperationContract]
+        int SendVoteResponse(int Id);
+        [OperationContract]
+        void ProclaimAsTheLeader();
+        [OperationContract]
+        void SendHeartBeat();
+        [OperationContract]
+        void RecieveHeartBeat(int id);
+        [OperationContract]
+        bool RecieveLeader(int id);
+        [OperationContract]
+        void ShutDownLeader();
+        [OperationContract]
+        void RestartTimer();
+    }
+
+}
